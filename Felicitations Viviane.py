9a419dd2b9a289af9701fc5dc1c6e68bf3c8f660
Simon JAHAN <135836049+SimonJahan@users.noreply.github.com>
2025-07-24 10:01:56 +0200

    Initial commit

---
+++ Felicitations Viviane.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("🎈 Félicitations Viviane")
+st.write("Félicitations pour l'obtention de ton année, Viviane! 🎉")
+
+st.image("https://media.licdn.com/dms/image/v2/D4D03AQF4raXOdU0FXg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1715677326268?e=1756339200&v=beta&t=hL1cJUkfKIslrydPOUqYxywM72mNqV4boFzfJ2dBo3I", caption="Bravo Viviane!", width=300)
+
+st.write("Nous sommes tous très fiers de toi et nous te souhaitons le meilleur pour la suite de tes études et ta carrière. Continue à briller ! 🌟")
+st.balloons()
